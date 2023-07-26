079b45e93ec0e887128954de9dd27f9b63fe4ac3
Milana Moroz <milanamoroz@MacBook-Air-Milana.local>
2023-07-26 12:52:23 +0300

    intro strings

---
+++ molly2507.py
@@ -0,0 +1,30 @@
+# my_cat = 'My cat'
+#
+# my_greeting = "I'm Molly"
+# my_greeting2 = 'I\'m \nMolly and \tI\'m a woman!!!'
+# print(my_greeting2)
+#
+# chunk = """ my long text """
+# print(chunk)
+#
+# myltiplicated = my_cat * 5
+# print(myltiplicated)
+# print('pon')
+#
+# summa = my_greeting + my_cat
+# print(summa)
+#
+# skier_unicode = '\U000026F7'
+# print(skier_unicode)
+
+string1 = 'My'
+string2 = 'name'
+string3 = 'is'
+string4 = 'really'
+string5 = 'long'
+
+sentence = string1 + ' ' + string2 + ' ' + string3 + ' ' + string4 + ' ' + string5 + '.'
+print(sentence)
+
+good_formed_sentence = f'{string1} {string2} {string3} {string4} {string5}.'
+print(good_formed_sentence)
